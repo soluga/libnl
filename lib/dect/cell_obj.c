accc75c463f8346d3b1fd6f112bfe07269601b52
Patrick McHardy <kaber@gw.localnet>
2009-12-04 11:04:51 +0100

    libnl: dect: add cell slave mode support
    Signed-off-by: Patrick McHardy <kaber@trash.net>

--- lib/dect/cell_obj.c
+++ lib/dect/cell_obj.c
@@ -204,6 +204,7 @@ nla_put_failure:
 
 static struct trans_tbl cell_flags[] = {
 	__ADD(DECT_CELL_CCP,	ccp)
+	__ADD(DECT_CELL_SLAVE,	slave)
 };
 
 char *nl_dect_cell_flags2str(uint32_t flags, char *buf, size_t len)
